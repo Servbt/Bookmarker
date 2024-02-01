ed1bc4665a5021e4c3dd53dc045014308c96ab31
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-01-31 22:14:43 -0500

    utils init and export default refactor

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+export default {
+	format_date: (date) => {
+		// Format date as MM/DD/YYYY
+		return date.toLocaleDateString();
+	},
+	randomNumber: (max) => {
+		return Math.floor(Math.random() * max) + 1;
+	},
+};
