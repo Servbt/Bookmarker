d8f67f110b002d1dc70f7675a83aab7fc0f4a3c8
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-02-03 22:26:08 -0500

    retrieve user data

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,9 @@
 const Sequelize = require('sequelize');
-// const dotenv = require('dotenv').config();
-
-// let sequelizes;
-
 
 const sequelize = new Sequelize('bookmarker', 'postgres', 'C@rrib3an!', {
   host: 'localhost',
   dialect: 'postgres',
   port: 5432,
 });
-
-// try {
-//   await sequelize.authenticate();
-//   console.log('Connection has been established successfully.');
-// } catch (error) {
-//   console.error('Unable to connect to the database:', error);
-// }
 
 module.exports = sequelize;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-// import User from '../models/User.js';
-// import Book from '../models/Book.js';
-// import Tag from '../models/Tag.js';
+const User = require('../models/User.js')
 
 router.get('/', (req, res) => {
     res.render('index.ejs');
@@ -12,9 +10,10 @@ router.get('/users', async (req, res) => {
     try {
       const userData = await User.findAll();
       const users = userData.map((user) => user.get({ plain: true }));
-  
+      
       res.status(200).json(users);
     } catch (err) {
+      console.log(err);
       res.status(500).json(err);
     }
   });
