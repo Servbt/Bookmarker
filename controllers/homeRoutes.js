707a1fcd08cd54c5a748a523c02af1c5d0c7fd97
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-02-05 18:24:25 -0500

    styling init and pagination init

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,24 +4,25 @@ const axios = require('axios').default;
 const { Book, User, Review, Tag } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('index.ejs');
+  res.render('landing.ejs');
 });
 
 // render login page
 router.get('/login', async (req, res) => {
   // redirect to home if user is logged in
   if (req.session.logged_in) {
-    res.redirect('/home');
+    res.render('home.ejs');
     return;
   }
-  res.render('login');
+  res.render('log-sign-link.ejs');
+
 });
 
 // render signup page
 router.get('/signup', async (req, res) => {
-  res.render('signup');
+  res.render('log-sign-link.ejs');
 });
 
 
 
-  module.exports = router;
+module.exports = router;
