2d01688fb5649d16a9a5d60d41bad810acd9dfaf
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-02-13 15:50:50 -0500

    seeder review and revision

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,14 @@
-// import { User, Book, Tag, Review }  from '../models/index.js';
-import User from '../models/User.js'
-import Book from '../models/Book.js'
-import Review from '../models/Review.js'
-// import sequelize from '../config/connection.js';
-// const sequelize = await import(`file:///C:/Users/arian/Documents/Development/My%20projects/BookMarker/config/connection`);
-import userData  from './userData.json' assert { type: "json" };
-import reviewData  from './reviewData.json' assert { type: "json" };
-import bookData  from './bookData.json' assert { type: "json" };
+const sequelize = require('../config/connection');
+const { User, Book, Tag, Review } = require('../models');
+
+const userData = require('./userData.json');
+// const bookData = require('./bookData.json');
+const reviewData = require('./reviewData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: false });
   const users = await User.bulkCreate(userData);
-  const books = await Book.bulkCreate(bookData);
+  // const books = await Book.bulkCreate(bookData);
   const reviews = await Review.bulkCreate(reviewData);
   // await User.bulkCreate(userData, {
   //   individualHooks: true,
