78b3951890aa02592a6a44e171506df3c418971d
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-08-07 00:01:30 -0400

    fix env strings

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+let sequelize;
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // Important for Heroku SSL
-    }
-  }
-});
-
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+      port: process.env.DB_PORT,
+    },
+  );
+}
 module.exports = sequelize;
+
