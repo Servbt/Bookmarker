1a21f6dde2a4f3beaa6ae0034a1bca2fe70bf178
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-08-06 23:50:52 -0400

    add configuration for sequelize to uses database_url env file

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  port: process.env.DB_PORT,
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Important for Heroku SSL
+    }
+  }
 });
 
 module.exports = sequelize;
